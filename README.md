f1a9efb3e38370aa9cf6f81e4c9e5b08cbb4e42a
Lukman Hakim <hlukman156@gmail.com>
2021-09-27 19:50:43 +0700

    ready, set, go!

---
+++ README.md
@@ -0,0 +1,4 @@
+## Lima ✨
+
+Lima merupakan singkatan dari Lukman Implentasi Algoritma, adalah repo untuk mendokumenstasikan seluruh tugas
+implentasi algoritma D3 RPLA di Telkom University.
---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukin Nama Kamu: ");
+        String nama = sc.nextLine();
+
+        System.out.print("Masukin NIM Kamu: ");
+        String nim = sc.nextLine();
+
+        System.out.print("Masukin IPK Kamu: ");
+        float ipk = sc.nextFloat();
+        sc.close();
+        
+        System.out.println();
+        System.out.println("Nama\t: " + nama);
+        System.out.println("NIM\t: " + nim);
+        System.out.println("IPK\t: " + ipk);
+        
+    }
+
+}
+
